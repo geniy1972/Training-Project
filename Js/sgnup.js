7f8378fdacac36ce4d3454b7cc5ad475f9d45717
Eugen Chernitsky <EugenChernitsky@coherentsolutions.com>
2020-01-16 17:10:15 +0300

    16012020

--- Js/sgnup.js
+++ Js/sgnup.js
@@ -1,22 +1,43 @@
 let Btn = document.getElementById('btn');
 Btn.addEventListener('click', valid);
 
+let emaiArray = localStorage.getItem('EmailStorage');
+if ( !emaiArray) {
+    emaiArray = [];
+}
+else {
+    emaiArray = JSON.parse(emaiArray);
+}
+
+
+let users = localStorage.getItem('usersStorage');
+if(!users) {
+    users = {};
+}
+users.name = localStorage.getItem('nameStorage');
+users.email = localStorage.getItem('EmailStorage');
+
 function valid() {
-    const name = document.getElementById('name');
-    const lastName = document.getElementById('lastName');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const confirmPassword = document.getElementById('confirmPassword');
+    let name = document.getElementById('name');
+    let lastName = document.getElementById('lastName');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let confirmPassword = document.getElementById('confirmPassword');
     const adr_pattern = /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}$/i;
+
+    
+    let errors = document.getElementsByClassName('name_error');
     
     let messages = false;
 
     if (name.value === '' || name.value == null) {
-        messages ='Name is required';
+        //messages ='Name is required';
+        errors[0].style.visibility = 'visible';
     }
 
     else if (lastName.value === '' || lastName.value == null) {
-        messages ='Last Name is required';
+        //messages ='Last Name is required';
+        errors[1].style.visibility = 'visible';
     }
 
     else if (adr_pattern.test(email.value) == false) {
@@ -44,11 +65,13 @@ function valid() {
     }
 
     else {
+        emaiArray.push(email.value);
         localStorage.setItem("nameStorage", name.value);
         localStorage.setItem("passwordStorage", password.value);
-        localStorage.setItem("EmailStorage", email.value);
+        localStorage.setItem("EmailStorage", JSON.stringify(emaiArray));
+        localStorage.setItem("usersStorage", users);
         window.location.assign("congSignUp.html");
-        console.log(localStorage)
+        //console.log(localStorage)
 
     }
 }
